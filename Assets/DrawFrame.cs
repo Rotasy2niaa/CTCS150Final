faa728909624d1064473199cdc8bdd73c226e9d6
rotasy2niaa@gmail.com <rotasy2niaa@gmail.com>
2025-04-17 21:18:56 -0700

    updated

--- Assets/DrawFrame.cs
+++ Assets/DrawFrame.cs
@@ -1,7 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class DrawFrame : MonoBehaviour
 {
     Mesh mesh;
@@ -9,25 +8,24 @@ public class DrawFrame : MonoBehaviour
     int[] triangles;
     public bool inDrawMode = false;
 
-
     void Start()
     {
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
-       
     }
 
     private void Update()
     {
+        float z = Camera.main.nearClipPlane + 10f;  // 比节点更远一点
+
 
         if (!inDrawMode)
         {
             if (Input.GetMouseButton(0))
             {
                 inDrawMode = true;
-                vertices = null;
                 vertices = new Vector3[4];
-                vertices[0] = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f));
+                vertices[0] = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, z));
             }
         }
 
@@ -35,14 +33,14 @@ public class DrawFrame : MonoBehaviour
         {
             if (Input.GetMouseButton(0))
             {
-                vertices[1] = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Camera.main.WorldToScreenPoint(vertices[0]).y, 10f));
-                vertices[3] = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.WorldToScreenPoint(vertices[0]).x, Input.mousePosition.y, 10f));
-                vertices[2] = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f));
-
+                vertices[1] = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Camera.main.WorldToScreenPoint(vertices[0]).y, z));
+                vertices[3] = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.WorldToScreenPoint(vertices[0]).x, Input.mousePosition.y, z));
+                vertices[2] = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, z));
             }
-            if( ((vertices[2].x > vertices[0].x) && (vertices[2].y < vertices[0].y)) ||
+
+            if (((vertices[2].x > vertices[0].x) && (vertices[2].y < vertices[0].y)) ||
                 ((vertices[2].x < vertices[0].x) && (vertices[2].y > vertices[0].y)))
-                triangles = new int[6] { 0, 1, 2, 0, 2, 3};
+                triangles = new int[6] { 0, 1, 2, 0, 2, 3 };
             else
                 triangles = new int[6] { 0, 3, 2, 0, 2, 1 };
         }
@@ -50,17 +48,52 @@ public class DrawFrame : MonoBehaviour
         if (Input.GetMouseButtonUp(0))
         {
             inDrawMode = false;
+            SelectNodesInFrame();
         }
 
         UpdateMesh();
     }
 
+    public LineRenderer lineRenderer;
+
     void UpdateMesh()
     {
         mesh.Clear();
+        if (vertices == null || vertices.Length < 3) return;
+
         mesh.vertices = vertices;
         mesh.triangles = triangles;
         mesh.RecalculateNormals();
+
+        if (lineRenderer != null && vertices.Length == 4)
+        {
+            lineRenderer.positionCount = 5;
+            lineRenderer.SetPosition(0, vertices[0]);
+            lineRenderer.SetPosition(1, vertices[1]);
+            lineRenderer.SetPosition(2, vertices[2]);
+            lineRenderer.SetPosition(3, vertices[3]);
+            lineRenderer.SetPosition(4, vertices[0]);  // 回到起点
+        }
+    }
+
+
+    void SelectNodesInFrame()
+    {
+        if (vertices == null || vertices.Length < 4) return;
+
+        Vector3 bottomLeft = new Vector3(Mathf.Min(vertices[0].x, vertices[2].x), Mathf.Min(vertices[0].y, vertices[2].y), 0);
+        Vector3 topRight = new Vector3(Mathf.Max(vertices[0].x, vertices[2].x), Mathf.Max(vertices[0].y, vertices[2].y), 0);
+
+        NodeScript[] allNodes = FindObjectsOfType<NodeScript>();
+
+        foreach (NodeScript node in allNodes)
+        {
+            Vector3 pos = node.transform.position;
+            bool isInside = pos.x >= bottomLeft.x && pos.x <= topRight.x &&
+                            pos.y >= bottomLeft.y && pos.y <= topRight.y;
+
+            node.Highlight(isInside);
+        }
     }
 
     public Mesh GetMesh()
@@ -68,4 +101,3 @@ public class DrawFrame : MonoBehaviour
         return mesh;
     }
 }
-
--- Assets/NodeScript.cs
+++ Assets/NodeScript.cs
@@ -1,11 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class NodeScript : MonoBehaviour
 {
     [SerializeField] float censusValue;
     [SerializeField] bool isCounted;
+
+    private Renderer rend;
 
     public float CensusValue
     {
@@ -17,5 +17,28 @@ public class NodeScript : MonoBehaviour
     {
         get { return isCounted; }
         set { isCounted = value; }
+    }
+
+    private void Awake()
+    {
+        // 获取本体或子物体上的 Renderer
+        rend = GetComponent<Renderer>();
+        if (rend == null)
+        {
+            rend = GetComponentInChildren<Renderer>();
+        }
+
+        if (rend == null)
+        {
+            Debug.LogWarning("No Renderer found on " + gameObject.name);
+        }
+    }
+
+    public void Highlight(bool state)
+    {
+        if (rend != null)
+        {
+            rend.material.color = state ? Color.yellow : Color.white;
+        }
     }
 }
